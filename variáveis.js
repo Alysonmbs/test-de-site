59a4d64b009bdfa9723af80c358d2b65ab866a1f
Limonation <121680045+Alysonmbs@users.noreply.github.com>
2025-02-22 16:44:48 -0300

    a

---
+++ variáveis.js
@@ -0,0 +1,17 @@
+const nome = "Alyson";
+const vivo = true;
+let idade = 18;
+
+const pessoa1 = {
+    nome,
+    vivo,
+    idade,
+}
+
+const pessoa2 = {
+    nome = "Yuri",
+    vivo = true,
+    idade = 11,
+}
+
+console.log
